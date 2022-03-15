d6ae309b4ce9af00cddfb64e2da3766e3da726f2
App Academy Student <email@email.com>
2022-03-15 16:19:44 -0400

    work on controllers

---
+++ music_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,6 @@
+class SessionsController < ApplicationController
+
+  # Write controller methods and the accompanying routes so that users can log in and log out. Should session be a singular resource?
+
+  #CELLL = current user, ensure_logged_in, login!, logout!, logged_in?
+end
--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,27 +1,28 @@
 class User < ApplicationRecord
   validates :email, :session_token, presence: true, uniqueness: true
-  validates :password_object, presence: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
   after_initialize :ensure_session_token
   attr_reader :password
-  
-  def self.find_by_credentials
+
+  def find_by_credentials(email, password)
     user = User.find_by(username:email)
+    
     if user && user.is_password?(password)
       user
     else
       nil
-    end 
+    end
   end
 
   def is_password?(password)
-    password_object = BCrypt::password.new(self.password_digest)
-
+    password_object = BCrypt::Password.new(self.password_digest)
     password_object.is_password?(password)
   end
 
   def password=(password)
     @password = password
-    self.password_digest = BCrypt::Password.new(password)
+    self.password_digest = BCrypt::Password.create(password)
     p 'in setter'
   end
 
@@ -29,10 +30,10 @@ class User < ApplicationRecord
     self.session_token ||= SecureRandom::urlsafe_base64
   end
 
-  def reset_session_token
+  def reset_session_token!
     self.session_token = SecureRandom::urlsafe_base64
 
     self.save!
     self.session_token
-  end  
+  end
 end
